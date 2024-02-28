109ff7d42a1c1c8890cc4f42f5c0ca60d5efd4f0
bar23354 <bar23354@uvg.edu.gt>
2024-02-27 20:32:50 -0600

    Primeros pasos (base)

--- Simulando.py
+++ Simulando.py
@@ -2,3 +2,15 @@
 # Algoritmos y Estructura de Datos - 40
 # Roberto Barreda - 23354
 
+import simpy
+import random
+import math
+
+RANDOM_SEED = 42
+Procesos_Nuevos = 100
+Procesos_entre_Intervalos = 10.0
+Min_Espera = 1
+Max_Espera = 3
+RAM_CAPACITY = 100
+CPU_SPEED = 3
+tiempos_finalizacion = []
